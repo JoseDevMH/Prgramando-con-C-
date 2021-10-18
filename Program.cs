0fad61630f197648a17f8d1195a464e1c8c6b62c
JoseDevMH <josedevmh@mail.com>
2021-10-18 17:06:52 -0500

    Fechas sumandole dias o meses o minutos, fecha y hora actual

--- Program.cs
+++ Program.cs
@@ -12,12 +12,21 @@ namespace ProgramandoCSharp
         {
             DateTime fecha = new DateTime(2004, 2, 10);
             DateTime fechaConHora = new DateTime(2021, 10, 18, 16, 40, 50);
-            
+            DateTime fechaHoy = new DateTime();
+            fechaHoy = DateTime.Now;
+
 
             Console.WriteLine("Fecha en que entre a mi primer trabajo formal de Informática: " + fecha.ToString());
             Console.WriteLine(fecha.ToString("dd/MM/yyyy"));
             Console.WriteLine(fecha.ToString("dd/MM/yy"));
             Console.WriteLine("Fecha con Hora: " + fechaConHora.ToString("dd/MM/yyyy hh:mm:ss"));
+
+            
+
+            Console.WriteLine(fecha.AddDays(45).ToString("dd/MM/yyyy"));
+            Console.WriteLine("Fecha de hoy: " + DateTime.Now);
+            Console.WriteLine("La fecha de hoy más 45 días: " + fechaHoy.AddDays(45).ToString("dd/MM/yyyy"));
+            Console.WriteLine(DateTime.Now.AddDays(45).ToString("dd/MM/yyyy"));
 
             
 
