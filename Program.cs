e5cbd9b3e4bc6b717e7c912cf9f50802859f2b5b
JoseDevMH <josedevmh@mail.com>
2021-10-18 17:04:56 -0500

    Algunos formatos de Fechas

--- Program.cs
+++ Program.cs
@@ -10,17 +10,21 @@ namespace ProgramandoCSharp
     {
         static void Main(string[] args)
         {
+            DateTime fecha = new DateTime(2004, 2, 10);
+            DateTime fechaConHora = new DateTime(2021, 10, 18, 16, 40, 50);
+            
+
+            Console.WriteLine("Fecha en que entre a mi primer trabajo formal de Informática: " + fecha.ToString());
+            Console.WriteLine(fecha.ToString("dd/MM/yyyy"));
+            Console.WriteLine(fecha.ToString("dd/MM/yy"));
+            Console.WriteLine("Fecha con Hora: " + fechaConHora.ToString("dd/MM/yyyy hh:mm:ss"));
+
+            
+
+            Console.ReadLine();
         }
 
-        class Persona
-        {
-            public string nombre;
-            private int edad;
+        
 
-            public int obtenerEdad()
-            {
-                return edad;
-            }
-        }
     }
 }
