7e39e8a19efff4fe334b014b4be5663c648a20c6
JoseDevMH <josedevmh@mail.com>
2021-10-18 16:32:11 -0500

    Inicio del Curso Programando en C # de Felipe Gavilanch

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramandoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        class Persona
+        {
+            public string nombre;
+            private int edad;
+
+            public int obtenerEdad()
+            {
+                return edad;
+            }
+        }
+    }
+}
